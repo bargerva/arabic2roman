090af7cb22b88100261eee9f2545ebc3d1f0e581
bargerva <bargerva@dukes.jmu.edu>
2020-05-07 17:23:37 -0400

    can correctly convert all numbers

--- arabic2roman_test.py
+++ arabic2roman_test.py
@@ -7,20 +7,20 @@ def describe_an_arabic2roman_program_that():
     """A program to convert Arabic numerals to Roman numerals."""
 
     def has_a_smoke_test():
-    """Make sure our testing infrastructure is working."""
+        """Make sure our testing infrastructure is working."""
     assert True == True
 
     def can_convert_1_to_I():
-    assert convert(1) == "I"
+        assert convert(1) == "I"
 
     def can_convert_2_to_II():
-    assert convert(2) == "II"
+        assert convert(2) == "II"
 
     def returns_I_II_or_III_if_remainder_of_division_by_5_is_1_to_3():
-    """
-        Test that our program ends in I, II, or III if the remainder
-        when dividing by 5 is 1, 2, or 3, respectively.
-    """
+        """
+            Test that our program ends in I, II, or III if the remainder
+            when dividing by 5 is 1, 2, or 3, respectively.
+        """
         assert convert(1)[-1:] == "I"
         assert convert(2)[-2:] == "II"
         assert convert(3)[-3:] == "III"
